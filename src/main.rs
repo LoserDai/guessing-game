0919268e8287ba79ab90b5916ec1eb06b70c38a5
LoserDai <15570237906@139.com>
2024-01-12 17:39:53 +0800

    update project :add loop + break

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,34 @@ fn main() {
     //随机生成一个1到100的数
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret_number is: {secret_number}");
-    println!("Please input your guess!!!");
+    //加入循环
+    loop {
+        println!("Please input your guess!!!");
 
-    //mut可以使一个变量可变
-    let mut guess = String::new();
+        //mut可以使一个变量可变
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line !");
+        //读取控制台打印的数据是多少
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line !");
 
-    let guess: u32 = guess.trim().parse().expect("please type a number!");
+        //优化：处理无效的输入
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed:{guess}");
+        println!("You guessed:{guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println! {"Too small!"},
-        Ordering::Equal => println! {"You Win!"},
-        Ordering::Greater => println! {"Too large!"},
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println! {"Too small!"},
+            Ordering::Greater => println! {"Too large!"},
+            //如果猜中了就跳出循环
+            Ordering::Equal => {
+                println!("You Win!!!");
+                break;
+            }
+        }
     }
 }
